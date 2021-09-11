7f1a3045267dd26fa36c62e4e99360b19cd62c1b
wildanptra <wildanptra03@gmail.com>
2021-09-11 14:00:41 +0700

    First Commit

---
+++ application/views/about.php
@@ -0,0 +1,196 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+
+<!-- Mirrored from maantheme.com/maansoft/maansoft/about.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 10 Sep 2021 08:48:49 GMT -->
+<head>	
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title>  Woodev - About </title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Place favicon.ico in the root directory -->
+	<link rel="shortcut icon" type="image/x-icon" href="assets/img/logo/favicon.png">
+	<!-- ========== Start Stylesheet ========== -->
+	<link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+	<link href="assets/css/fontawesome.min.css" rel="stylesheet" />
+	<link href="assets/css/owl.carousel.min.css" rel="stylesheet" />
+    <link href="assets/css/jquery.fancybox.min.css" rel="stylesheet" />
+	<link href="assets/css/owl.theme.default.min.css" rel="stylesheet" />
+	<link href="assets/css/animate.css" rel="stylesheet" />
+	<link href="assets/css/flaticon-set.css" rel="stylesheet" />
+	<link href="assets/css/themify-icons.css" rel="stylesheet" />
+	<link href="style.css" rel="stylesheet">
+	<link href="assets/css/responsive.css" rel="stylesheet" />
+	<!-- ========== End Stylesheet ========== -->
+  
+</head>
+
+<body id="bdy">		
+
+    
+	<!-- Start header
+    ============================================= -->
+    <header class="header bg-2">
+		<div class="main-navigation">
+			<nav id="navbar_top" class="navbar navbar-expand-lg">
+				<div class="container g-0">
+					<a class="navbar-brand" href="#">
+						<img src="assets/img/logo/woodev-logo.png" class="logo-display" alt="MaanSoft">
+						<img src="assets/img/logo/woodev-logo.png" class="logo-scrolled" alt="MaanSoft">
+					</a>
+					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main_nav" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"><i class="ti-menu-alt"></i></span>
+					</button>
+					<div class="collapse navbar-collapse" id="main_nav">
+						<ul class="navbar-nav ms-auto">
+							<li class="nav-item"><a class="nav-link" href="index.php"> Home </a></li>
+							<li class="nav-item"><a class="nav-link" href="about.php"> About </a></li>
+							<li class="nav-item"><a class="nav-link" href="blog.php"> Blog </a></li>
+							<li class="nav-item"><a class="nav-link" href="contact.php"> Contact </a></li>
+						</ul>
+					</div> <!-- navbar-collapse.// -->
+                    <div class="header-cart-btn">
+                        <a href="#" class="btn-6">+62895 1306 2754 <i class="fab fa-whatsapp" style="color:#fff;"></i> </a>
+                    </div>
+				</div> <!-- container -->
+			</nav>
+		</div>
+    </header>
+    <!-- End header -->
+	    
+	<main class="main">
+        
+        <!-- Start Breadcrumb
+		============================================= -->
+		<div class="site-breadcrumb" style="background: url(assets/img/breadcrumb/breadcrumb.jpg)">
+			<div class="container">
+			<h2 class="breadcrumb-title">About Us</h2>
+				<ul class="breadcrumb-menu clearfix">
+					<li><a href="index.php">Home</a></li>
+					<li class="active">About</li>
+				</ul>
+			</div>
+		</div>
+        <!-- End Breadcrumb -->
+        
+        <!-- Start About
+		============================================= -->
+        <div class="about-area de-padding">
+            <div class="container">
+                <div class="about-wpr grid-2">
+                    <div class="about-left style-2">
+                        <h2 class="about-title">
+                            Let’s see About Woodev.
+                        </h2>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                        </p>
+                        <a href="#" class="btn-6">Load More</a>
+                    </div>
+                    <div class="about-right">
+                        <div class="about-right-pic">
+                            <!-- MaanSoft  -->
+							<img class="about-1 animate-float-bob-x" src="assets/img/logo/favicon.png" alt="MaanSoft">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End About -->
+
+        
+	</main>	
+		
+	<div class="clearfix"></div>
+	
+	<!-- Start Footer
+	============================================= -->
+	<footer>
+		<div class="footer-widget">
+			<div class="container">
+				<div class="footer-widget-wpr de-padding">
+                    <div class="row f-cs g-5">
+                        <div class="col-xl-4 col-md-6">
+                            <div class="footer-widget-box about-us">
+                                <h4 class="footer-widget-title">About Us</h4>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                </p>
+                                <ul class="footer-social">
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-xl-4 col-md-6">
+                            <div class="footer-widget-box list">
+                                <h4 class="footer-widget-title">Maps</h4>
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63417.027798256306!2d106.73072223162252!3d-6.576547064361255!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69c4f738972703%3A0x2c04739fc32b8e0e!2sKec.%20Bogor%20Bar.%2C%20Kota%20Bogor%2C%20Jawa%20Barat!5e0!3m2!1sid!2sid!4v1631269284224!5m2!1sid!2sid" width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                            </div>
+                        </div>
+                        <div class="col-xl-4 col-md-6">
+                            <div class="footer-widget-box f-contact">
+                                <h4 class="footer-widget-title">Quick Contact</h4>
+                                <p class="mb-0">
+                                    Excepteur sint occaecat cupidatat non proident, sunt in 
+                                </p>
+                                <div class="footer-contact mt-20 mb-20">
+                                    <form>
+                                        <input class="form-control input-style-1" type="email" placeholder="Your Mail">
+                                        <button type="submit" class="btn-sub">
+                                            <i><img src="assets/img/icons/submit.png" alt="MaanSoft"></i>
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+				</div>
+			</div>
+            <div class="copyright">
+                <p class="mb-0">© Copyright Wildan Pratama Putra 2021. All Right Reserved.</p>
+            </div>
+		</div>
+	</footer>	
+	<!-- End Footer-->	
+	
+	<!-- Start Scroll top
+	============================================= -->
+	<a href="#bdy" id="scrtop" class="smooth-menu"><i class="ti-arrow-up"></i></a>
+	<!-- End Scroll top-->
+	
+	<!-- Start Range -->
+	<div class="widget slide-range d-none">
+		<h5 class="work-title">Filter by price</h5>
+		<div class="slidecontainer">
+			<input type="range" min="1" max="100" value="49" class="slider" id="myRange">
+			<p>0 - <span id="demo"></span></p>
+		</div>
+	</div>
+	<!-- End Range-->
+	
+  	<!-- jQuery Frameworks
+    ============================================= -->
+	<script src="assets/js/jquery-3.5.0.min.js"></script>
+	<script src="assets/js/jquery.easing.min.js"></script>
+	<script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/bootstrap-menu.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/isotope.pkgd.min.js"></script>
+    <script src="assets/js/jquery.fancybox.js"></script>
+    <script src="assets/js/jquery.appear.js"></script>
+	<script src="assets/js/count-to.js"></script>
+	<script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/main.js"></script>
+	
+</body>
+
+
+<!-- Mirrored from maantheme.com/maansoft/maansoft/about.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 10 Sep 2021 08:48:50 GMT -->
+</html>
---
+++ application/views/contact.php
@@ -0,0 +1,229 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+
+<!-- Mirrored from maantheme.com/maansoft/maansoft/about.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 10 Sep 2021 08:48:49 GMT -->
+<head>	
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title>  Woodev - Contact </title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Place favicon.ico in the root directory -->
+	<link rel="shortcut icon" type="image/x-icon" href="assets/img/logo/favicon.png">
+	<!-- ========== Start Stylesheet ========== -->
+	<link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+	<link href="assets/css/fontawesome.min.css" rel="stylesheet" />
+	<link href="assets/css/owl.carousel.min.css" rel="stylesheet" />
+    <link href="assets/css/jquery.fancybox.min.css" rel="stylesheet" />
+	<link href="assets/css/owl.theme.default.min.css" rel="stylesheet" />
+	<link href="assets/css/animate.css" rel="stylesheet" />
+	<link href="assets/css/flaticon-set.css" rel="stylesheet" />
+	<link href="assets/css/themify-icons.css" rel="stylesheet" />
+	<link href="style.css" rel="stylesheet">
+	<link href="assets/css/responsive.css" rel="stylesheet" />
+	<!-- ========== End Stylesheet ========== -->
+  
+</head>
+
+<body id="bdy">		
+
+    
+	<!-- Start header
+    ============================================= -->
+    <header class="header bg-2">
+		<div class="main-navigation">
+			<nav id="navbar_top" class="navbar navbar-expand-lg">
+				<div class="container g-0">
+					<a class="navbar-brand" href="#">
+						<img src="assets/img/logo/woodev-logo.png" class="logo-display" alt="MaanSoft">
+						<img src="assets/img/logo/woodev-logo.png" class="logo-scrolled" alt="MaanSoft">
+					</a>
+					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main_nav" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"><i class="ti-menu-alt"></i></span>
+					</button>
+					<div class="collapse navbar-collapse" id="main_nav">
+						<ul class="navbar-nav ms-auto">
+							<li class="nav-item"><a class="nav-link" href="index.php"> Home </a></li>
+							<li class="nav-item"><a class="nav-link" href="about.php"> About </a></li>
+							<li class="nav-item"><a class="nav-link" href="blog.php"> Blog </a></li>
+							<li class="nav-item"><a class="nav-link" href="contact.php"> Contact </a></li>
+						</ul>
+					</div> <!-- navbar-collapse.// -->
+                    <div class="header-cart-btn">
+                        <a href="#" class="btn-6">+62895 1306 2754 <i class="fab fa-whatsapp" style="color:#fff;"></i> </a>
+                    </div>
+				</div> <!-- container -->
+			</nav>
+		</div>
+    </header>
+    <!-- End header -->
+	    
+	<main class="main">
+        
+        <!-- Start Breadcrumb
+		============================================= -->
+		<div class="site-breadcrumb" style="background: url(assets/img/breadcrumb/breadcrumb.jpg)">
+			<div class="container">
+			<h2 class="breadcrumb-title">Contact Us</h2>
+				<ul class="breadcrumb-menu clearfix">
+					<li><a href="index.php">Home</a></li>
+					<li class="active">Contact Us</li>
+				</ul>
+			</div>
+		</div>
+        <!-- End Breadcrumb -->
+        
+        <!-- Start Contactg US
+		============================================= -->
+		<div class="contact-us de-padding">
+			<div class="container">
+				<div class="contact-us-wpr">
+					<div class="row ps g-5">
+						<div class="col-lg-8">
+							<div class="contact-us-content wow fadeInUp">
+								<h2 class="contact-us-title mb-50">
+									Always ready for your solution
+								</h2>
+								<form class="row g-5">
+									<div class="col-md-6">
+										<input type="text" class="form-control input-style-2" placeholder="First Name">
+									</div>
+									<div class="col-md-6">
+										<input type="text" class="form-control input-style-2" placeholder="Last Name">
+									</div>
+									<div class="col-md-12">
+										<input type="text" class="form-control input-style-2" placeholder="Subject">
+									</div>
+									<div class="col-md-12">
+										<input type="email" class="form-control input-style-2" placeholder="Email">
+									</div>
+									<div class="col-12">
+										<textarea class="form-control input-style-2" placeholder="Message"></textarea>
+									</div>
+									<div class="col-12">
+										<div class="login-btns">
+											<button type="submit" class="btn-6 login-btn">
+												Contact Us
+											</button>
+										</div>
+									</div>
+								</form>
+							</div>
+						</div>
+						<div class="col-lg-4">
+							<div class="contact-us-sidebar">
+								<div class="widget contact-us">
+									<div class="contact-us-shape">
+										<img src="assets/img/contact/contact-us-shape.png" alt="MaanSoft">
+									</div>
+									<h4 class="widget-contact-title">
+										Have any Question?
+									</h4>
+									<p class="mb-30">
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+									</p>
+									<a href="#" class="btn-6">Submit A Ticket</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- End Contact Us -->
+        
+	</main>	
+		
+	<div class="clearfix"></div>
+	
+	<!-- Start Footer
+	============================================= -->
+	<footer>
+		<div class="footer-widget">
+			<div class="container">
+				<div class="footer-widget-wpr de-padding">
+                    <div class="row f-cs g-5">
+                        <div class="col-xl-4 col-md-6">
+                            <div class="footer-widget-box about-us">
+                                <h4 class="footer-widget-title">About Us</h4>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                </p>
+                                <ul class="footer-social">
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-xl-4 col-md-6">
+                            <div class="footer-widget-box list">
+                                <h4 class="footer-widget-title">Maps</h4>
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63417.027798256306!2d106.73072223162252!3d-6.576547064361255!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69c4f738972703%3A0x2c04739fc32b8e0e!2sKec.%20Bogor%20Bar.%2C%20Kota%20Bogor%2C%20Jawa%20Barat!5e0!3m2!1sid!2sid!4v1631269284224!5m2!1sid!2sid" width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                            </div>
+                        </div>
+                        <div class="col-xl-4 col-md-6">
+                            <div class="footer-widget-box f-contact">
+                                <h4 class="footer-widget-title">Quick Contact</h4>
+                                <p class="mb-0">
+                                    Excepteur sint occaecat cupidatat non proident, sunt in 
+                                </p>
+                                <div class="footer-contact mt-20 mb-20">
+                                    <form>
+                                        <input class="form-control input-style-1" type="email" placeholder="Your Mail">
+                                        <button type="submit" class="btn-sub">
+                                            <i><img src="assets/img/icons/submit.png" alt="MaanSoft"></i>
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+				</div>
+			</div>
+            <div class="copyright">
+                <p class="mb-0">© Copyright Wildan Pratama Putra 2021. All Right Reserved.</p>
+            </div>
+		</div>
+	</footer>	
+	<!-- End Footer-->	
+	
+	<!-- Start Scroll top
+	============================================= -->
+	<a href="#bdy" id="scrtop" class="smooth-menu"><i class="ti-arrow-up"></i></a>
+	<!-- End Scroll top-->
+	
+	<!-- Start Range -->
+	<div class="widget slide-range d-none">
+		<h5 class="work-title">Filter by price</h5>
+		<div class="slidecontainer">
+			<input type="range" min="1" max="100" value="49" class="slider" id="myRange">
+			<p>0 - <span id="demo"></span></p>
+		</div>
+	</div>
+	<!-- End Range-->
+	
+  	<!-- jQuery Frameworks
+    ============================================= -->
+	<script src="assets/js/jquery-3.5.0.min.js"></script>
+	<script src="assets/js/jquery.easing.min.js"></script>
+	<script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/bootstrap-menu.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/isotope.pkgd.min.js"></script>
+    <script src="assets/js/jquery.fancybox.js"></script>
+    <script src="assets/js/jquery.appear.js"></script>
+	<script src="assets/js/count-to.js"></script>
+	<script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/main.js"></script>
+	
+</body>
+
+
+<!-- Mirrored from maantheme.com/maansoft/maansoft/about.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 10 Sep 2021 08:48:50 GMT -->
+</html>
